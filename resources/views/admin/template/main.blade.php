bda04193503898f239abe819451d9c1f74179a1f
Gustavo Adolfo Muñoz Reyes <Gustavo Adolfo Muoz Reyes>
2016-01-03 12:11:30 -0500

    Inicio del blog

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="{{ asset('plugins/materialize/css/materialize.css') }}">
+	<title>@yield('title','Default') | Panel de Administración</title>
+</head>
+<body>
+	
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.template.main')
+@section('title','Inicio')
+<h1>hola esto es materialize</h1>
+
+<a href="" class="waves-effect waves-light btn-large">Button</a>
